c45b656f45d7bd6fffd28b9da6e2b3ae135e29d6
AbdiwakB <abdiwakbek3226@gmail.com>
2023-04-30 13:00:41 +0300

    stepper motor commit

---
+++ StepperTest.ino
@@ -0,0 +1,43 @@
+
+#include <Stepper.h>
+
+const int stepsPerRevolution = 500;  // change this to fit the number of steps per revolution
+// for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+
+void setup() {
+  // set the speed at 60 rpm:
+  myStepper.setSpeed(60);
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+
+void loop() {
+
+//rate range: from 1 to 25
+// volume range: according to belt size
+forward(1, 20);
+delay(1000);
+backward(1,20);
+delay(1000);
+
+}
+
+void forward(int rate, int volume){
+    for(int i=1; i<=20; i++){
+    myStepper.step(volume);
+    delay(rate);
+    }
+  }
+
+void backward(int rate, int volume){
+    for(int i=1; i<=20; i++){
+    myStepper.step(-volume);
+    delay(rate);
+    }
+  }
+
+  
+
